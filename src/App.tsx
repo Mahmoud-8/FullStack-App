ee694409398018bffc8f44815e7010b9df9af57b
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-09-25 14:43:39 +0200

    done with route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import './App.css'
-import Header from './components/Header'
-import Product from './pages/Product'
-import img from './assets/img.avif'
+import './App.css';
+import Header from './components/Header';
+import Product from './pages/Product';
+import img from './assets/img.avif';
+import { Route, Routes, Outlet } from 'react-router-dom';
+import Checkout from './pages/Checkout';
 
-function App() {
-
+function Layout() {
   return (
     <>
-   <Header title='WEBROAD' />
-    <Product title='Complete Web Development' description='Learn React with Fullstack' images={[{ src: img, alt: 'Thumbnail'}]} price={12.99} />
+      <Header title='WEBROAD' />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <Product
+              title='Complete Web Development '
+              description='Learn React with Fullstack Complete Web Development Complete Web Development Complete Web Development Complete Web Development Complete Web Development'
+              images={[{ src: img, alt: 'Thumbnail' }]}
+              price={12.99}
+            />
+          }
+        />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="success" element={<div>Success</div>} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Checkout({}: Props) {
+  return (
+    <div>Checkout</div>
+  )
+}
+
+export default Checkout
